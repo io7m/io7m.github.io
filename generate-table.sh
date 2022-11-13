9568348ac150baf0ee079d073f1ceeaa3c7d89d4
Mark Raynsford <code@io7m.com>
2022-11-13 21:12:33 +0000

    Regenerate

--- generate-table.sh
+++ generate-table.sh
@@ -5,9 +5,10 @@ cat <<EOF
   <thead>
     <tr>
       <th>Project</th>
-      <th>OpenJDK LTS, Linux</th>
       <th>OpenJDK Current, Linux</th>
+      <th>OpenJDK LTS, Linux</th>
       <th>OpenJDK Current, Windows</th>
+      <th>OpenJDK LTS, Windows</th>
     </tr>
   </thead>
   <tbody>
@@ -18,9 +19,10 @@ do
   cat <<EOF
   <tr>
     <td><a href="https://www.github.com/io7m/${PROJECT}">${PROJECT}</a></td>
-    <td><a href="https://github.com/io7m/${PROJECT}/actions?query=workflow%3Amain-openjdk_lts-linux"><img src="https://img.shields.io/github/workflow/status/io7m/${PROJECT}/main-openjdk_lts-linux"/></a></td>
-    <td><a href="https://github.com/io7m/${PROJECT}/actions?query=workflow%3Amain-openjdk_current-linux"><img src="https://img.shields.io/github/workflow/status/io7m/${PROJECT}/main-openjdk_current-linux"/></a></td>
-    <td><a href="https://github.com/io7m/${PROJECT}/actions?query=workflow%3Amain-openjdk_current-windows"><img src="https://img.shields.io/github/workflow/status/io7m/${PROJECT}/main-openjdk_current-windows"/></a></td>
+    <td><a href="https://github.com/io7m/${PROJECT}/actions?query=workflow%3Amain.linux.temurin.current"><img src="https://img.shields.io/github/workflow/status/io7m/${PROJECT}/main.linux.temurin.current"/></a></td>
+    <td><a href="https://github.com/io7m/${PROJECT}/actions?query=workflow%3Amain.linux.temurin.lts"><img src="https://img.shields.io/github/workflow/status/io7m/${PROJECT}/main.linux.temurin.lts"/></a></td>
+    <td><a href="https://github.com/io7m/${PROJECT}/actions?query=workflow%3Amain.windows.temurin.current"><img src="https://img.shields.io/github/workflow/status/io7m/${PROJECT}/main.windows.temurin.current"/></a></td>
+    <td><a href="https://github.com/io7m/${PROJECT}/actions?query=workflow%3Amain.windows.temurin.lts"><img src="https://img.shields.io/github/workflow/status/io7m/${PROJECT}/main.windows.temurin.lts"/></a></td>
   </tr>
 EOF
 done
