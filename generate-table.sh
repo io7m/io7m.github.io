456e9fc82fb56cbeb340bb26b54299efe1e5484c
Mark Raynsford <code@io7m.com>
2024-03-29 23:25:49 +0000

    Update projects

--- generate-table.sh
+++ generate-table.sh
@@ -19,10 +19,10 @@ do
   cat <<EOF
   <tr>
     <td><a href="https://www.github.com/io7m/${PROJECT}">${PROJECT}</a></td>
-    <td><a href="https://github.com/io7m/${PROJECT}/actions?query=workflow%3Amain.linux.temurin.current"><img src="https://img.shields.io/github/workflow/status/io7m/${PROJECT}/main.linux.temurin.current"/></a></td>
-    <td><a href="https://github.com/io7m/${PROJECT}/actions?query=workflow%3Amain.linux.temurin.lts"><img src="https://img.shields.io/github/workflow/status/io7m/${PROJECT}/main.linux.temurin.lts"/></a></td>
-    <td><a href="https://github.com/io7m/${PROJECT}/actions?query=workflow%3Amain.windows.temurin.current"><img src="https://img.shields.io/github/workflow/status/io7m/${PROJECT}/main.windows.temurin.current"/></a></td>
-    <td><a href="https://github.com/io7m/${PROJECT}/actions?query=workflow%3Amain.windows.temurin.lts"><img src="https://img.shields.io/github/workflow/status/io7m/${PROJECT}/main.windows.temurin.lts"/></a></td>
+    <td><a href="https://github.com/io7m/${PROJECT}/actions?query=workflow%3Amain.linux.temurin.current"><img src="https://img.shields.io/github/actions/workflow/status/io7m/${PROJECT}/main.linux.temurin.current.yml"/></a></td>
+    <td><a href="https://github.com/io7m/${PROJECT}/actions?query=workflow%3Amain.linux.temurin.lts"><img src="https://img.shields.io/github/actions/workflow/status/io7m/${PROJECT}/main.linux.temurin.lts"/></a></td>
+    <td><a href="https://github.com/io7m/${PROJECT}/actions?query=workflow%3Amain.windows.temurin.current"><img src="https://img.shields.io/github/actions/workflow/status/io7m/${PROJECT}/main.windows.temurin.current"/></a></td>
+    <td><a href="https://github.com/io7m/${PROJECT}/actions?query=workflow%3Amain.windows.temurin.lts"><img src="https://img.shields.io/github/actions/workflow/status/io7m/${PROJECT}/main.windows.temurin.lts"/></a></td>
   </tr>
 EOF
 done
